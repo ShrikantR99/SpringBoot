d4a06821d81db52cb7e06f0cb24f0363571e3019
Shrikant Ravalukedari <133002244+ShrikantR99@users.noreply.github.com>
2024-06-05 18:08:07 +0530

    Add files via upload

---
+++ SpringProject5/src/main/java/com/Spring/example5/SpringProject5/Person.java
@@ -0,0 +1,33 @@
+package com.Spring.example5.SpringProject5;
+
+
+
+public class Person {
+
+	Mobile mob;
+
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(Mobile mob) {
+		super();
+		this.mob = mob;
+	}
+
+	public Mobile getMob() {
+		return mob;
+	}
+
+	public void setMob(Mobile mob) {
+		this.mob = mob;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [mob=" + mob + "]";
+	}
+	
+
+}
